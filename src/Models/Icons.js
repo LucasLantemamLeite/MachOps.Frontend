b84f810415706fd8b92ec2e4182e779b840c54f9
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-26 21:38:56 -0300

    feat: cria o objeto para centralizar icones

---
+++ src/Models/Icons.js
@@ -0,0 +1,17 @@
+import Forklift from "../Assets/Icons/Forklift.svg";
+import Loader from "../Assets/Icons/Loader.svg";
+import Excavator from "../Assets/Icons/Excavator.svg";
+import Bulldozer from "../Assets/Icons/Bulldozer.svg";
+import Crane from "../Assets/Icons/Crane.svg";
+import AGV from "../Assets/Icons/AGV.svg";
+import Skid from "../Assets/Icons/Skid.svg";
+
+export const Icons = {
+  ForkliftIcon: Forklift,
+  LoaderIcon: Loader,
+  ExcavatorIcon: Excavator,
+  BulldozerIcon: Bulldozer,
+  CraneIcon: Crane,
+  AGVIcon: AGV,
+  SkidIcon: Skid,
+};
