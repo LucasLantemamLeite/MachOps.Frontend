eeb390768bef27861ca71b052fd4d34906424a64
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-26 21:53:34 -0300

    feat: cria o componente da barra de navegação

---
+++ src/Sections/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import { Image } from "../../Components/Image";
+import "./NavBarStyle.scss";
+
+export function NavBar() {
+  return (
+    <>
+      <nav>
+        <Image staticImage={"AppLogoIcon"} width="500px" />
+      </nav>
+    </>
+  );
+}
