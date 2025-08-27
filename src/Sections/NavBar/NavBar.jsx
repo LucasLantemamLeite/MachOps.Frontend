631e485c01c0293228e1a5c6dcb571d4db25990d
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-26 22:28:36 -0300

    feat: cria a estrutura na barra de navegação com componentes

--- src/Sections/NavBar/NavBar.jsx
+++ src/Sections/NavBar/NavBar.jsx
@@ -1,11 +1,20 @@
-import { Image } from "../../Components/Image";
+import { Image } from "../../Shared/Components/Image";
+import { Input } from "../../Shared/Components/Input";
 import "./NavBarStyle.scss";
 
 export function NavBar() {
   return (
     <>
       <nav>
-        <Image staticImage={"AppLogoIcon"} width="500px" />
+        <div>
+          <Image staticImage={"AppLogoIcon"} width="170px" />
+        </div>
+        <div className="navbar__div-filter">
+          <div>
+            <Image staticImage={"SearchIcon"} width="30px" />
+          </div>
+          <Input placeholder={"Busque uma máquina aqui..."} />
+        </div>
       </nav>
     </>
   );
