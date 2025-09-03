cedad5ce7cc3875067430f1f3f3f3560e22ba463
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-02 22:23:23 -0300

    feat: cria o component de input para entrada de dados

---
+++ app/components/InputComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <label v-if="textLabel && id" :for="id">{{ textLabel }}</label>
+  <input :id="id" :name="name" :type="type" :placeholder="placeHolder" />
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    id?: string;
+    name?: string;
+    textLabel?: string;
+    placeHolder?: string;
+    type?: string;
+    initialValue?: string;
+  }>(),
+  {
+    type: "text",
+    placeHolder: "",
+    initialValue: "",
+  }
+);
+</script>
