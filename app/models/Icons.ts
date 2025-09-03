fe579856c2ff5bbab699eac2411105af4de59f8c
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-02 22:38:23 -0300

    feat: adiciona o novo icone na central

--- app/models/Icons.ts
+++ app/models/Icons.ts
@@ -1,7 +1,9 @@
 import AppLogo from "~/Assets/Icons/AppLogo.svg";
 import Search from "~/Assets/Icons/Search.svg";
+import Add from "~/Assets/Icons/Add.svg";
 
 export const Icons = {
   AppLogoIcon: AppLogo,
   SearchIcon: Search,
+  AddIcon: Add,
 };
