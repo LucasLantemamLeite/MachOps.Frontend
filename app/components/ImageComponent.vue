4371baf12932cf0f8f67f2128af5890a8b10ecb4
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-02 21:35:13 -0300

    feat: cria o componente de imagens

---
+++ app/components/ImageComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <ClientOnly>
+    <img v-if="staticImg" :src="Icons[staticImg]" :alt="alt" :class="className" />
+  </ClientOnly>
+</template>
+
+<script setup lang="ts">
+import { ClientOnly } from "#components";
+import { Icons } from "~/models/Icons";
+
+defineProps<{
+  staticImg?: keyof typeof Icons | null;
+  alt?: string | undefined;
+  className?: string | null;
+}>();
+</script>
