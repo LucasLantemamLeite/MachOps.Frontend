f08b15f18fe5958366576070b201ba10da3982a7
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-03 20:41:09 -0300

    feat: chama o componente de lista de máquinas

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,10 @@
 <template>
   <NavBar />
+  <MachinesList />
 </template>
 
 <script setup lang="ts">
+import MachinesList from "~/sections/MachinesList.vue";
 import NavBar from "~/sections/NavBar.vue";
 </script>
 
