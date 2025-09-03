fe5efa2774b905d2ad499ab83db4ea5739dfe427
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-02 22:22:58 -0300

    feat: importa o icone de pesquisa

--- app/models/Icons.ts
+++ app/models/Icons.ts
@@ -1,5 +1,7 @@
-import AppLogo from "../Assets/Icons/AppLogo.svg";
+import AppLogo from "~/Assets/Icons/AppLogo.svg";
+import Search from "~/Assets/Icons/Search.svg";
 
 export const Icons = {
   AppLogoIcon: AppLogo,
+  SearchIcon: Search,
 };
