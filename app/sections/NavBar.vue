ab435ebad7af5ca69f3302028c10fce1e55eda2f
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-02 21:35:41 -0300

    feat: cria a seção NavBar

---
+++ app/sections/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav>
+    <ImageComponent staticImg="AppLogoIcon" />
+    <div>
+      <p>caixa de pesquisa</p>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import ImageComponent from "~/components/ImageComponent.vue";
+</script>
+
+<style lang="scss">
+@use "../GlobalStyle.scss" as Style;
+
+nav {
+  display: grid;
+  padding: 1.5rem 1.2rem;
+  grid-template-columns: 0.1fr 1fr 0.1fr;
+  background-color: Style.$purple-primary;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0rem 0.2rem 0.6rem rgba(0, 0, 0, 0.418);
+
+  & img {
+    width: 20rem;
+  }
+
+  & div {
+    display: flex;
+    justify-content: center;
+    align-self: center;
+  }
+}
+</style>
