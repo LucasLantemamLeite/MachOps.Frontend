e88510068ae0ae86da085d050863af733b6c933b
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-06 21:00:13 -0300

    refactor: faz a organização do novo componente dividido

---
+++ app/sections/MachinesView/MachinesView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="machinelist__div-container">
+    <div class="machinelist__div-add-card">
+      <ImageComponent staticImg="AddIcon" />
+    </div>
+
+    <MachineList />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ImageComponent from "~/components/ImageComponent.vue";
+import MachineList from "./Components/MachinesList.vue";
+import "./MachinesViewStyle.scss";
+</script>
