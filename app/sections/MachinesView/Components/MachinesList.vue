c7a95c2dfaef3cb45d9cea8f0809a811f8eff02c
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-06 21:20:08 -0300

    feat: adiciona o estilo do componente único

--- app/sections/MachinesView/Components/MachinesList.vue
+++ app/sections/MachinesView/Components/MachinesList.vue
@@ -5,7 +5,7 @@
       <hr />
     </div>
 
-    <ImageComponent :type="MachineType" :imgKey="machine.machineType" />
+    <ImageComponent className="machineslist__div-mach-type" :type="MachineType" :imgKey="machine.type" />
   </div>
 </template>
 
@@ -21,3 +21,46 @@ onMounted(async () => {
   machines.value = result;
 });
 </script>
+
+<style lang="scss">
+@use "../../../GlobalStyle.scss" as Style;
+
+.machineslist__div-mach-card {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  height: 30rem;
+  cursor: pointer;
+  border-radius: Style.$default-border-radius;
+  background-color: Style.$purple-primary;
+  box-shadow: 0.2rem 0.3rem 0.4rem rgba(0, 0, 0, 0.3);
+  transition: 300ms ease;
+
+  &:hover {
+    transform: translateX(3px);
+  }
+}
+
+.machineslist__div-mach-title {
+  position: absolute;
+  top: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  p {
+    font-size: Style.$font-lg * 1.4;
+  }
+
+  hr {
+    width: 20rem;
+    background-color: white;
+    height: 0.3rem;
+  }
+}
+
+.machineslist__div-mach-type {
+  width: 14rem;
+}
+</style>
