b9f8b1e4752b6a9dda6917338804b1a236e3629e
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-06 22:51:03 -0300

    feat: dá acesso aos provider na página raiz da aplicação

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,17 @@
 <template>
   <NavBar />
-  <MachinesView />
+  <NotificationProvider>
+    <LoadingProvider>
+      <MachinesView />
+    </LoadingProvider>
+  </NotificationProvider>
 </template>
 
 <script setup lang="js">
 import NavBar from "~/Sections/NavBar/NavBar.vue";
 import MachinesView from "~/Sections/MachinesView/MachinesView.vue";
+import LoadingProvider from "~/Provider/LoadingProvider/LoadingProvider.vue";
+import NotificationProvider from "~/Provider/NotificationProvider/NotificationProvider.vue";
 </script>
 
 <style></style>
