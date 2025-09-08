58c7458a1887bf42ea2f52e3500327ac7c00e13c
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-08 11:19:14 -0300

    feat: cria o componente isolado para nome da máquina

---
+++ app/sections/CreateMachine/components/CreateMachineName.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <InputComponent className="createmachine__input-name" id="name" name="name" textLabel="Nome:" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import InputComponent from "~/components/InputComponent.vue";
+</script>
+
+<style lang="scss">
+@use "../../../GlobalStyle.scss" as Style;
+
+img {
+  width: 20rem;
+}
+
+div {
+  display: flex;
+  flex-direction: column;
+}
+
+.createmachine__input-name {
+  width: 35rem;
+  padding: 0.7rem;
+  border: none;
+  outline: none;
+  border-radius: Style.$default-border-radius;
+}
+</style>
