d5c25d3fe37c2cb88a8e5b635deca28f486a5a8b
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-08 09:42:54 -0300

    feat: cria o component de titulo da máquina

---
+++ app/sections/MachinesView/Components/MachineTitle.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>{{ machineName }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ machineName: string }>();
+</script>
+
+<style lang="scss" scoped>
+@use "../../../GlobalStyle.scss" as Style;
+
+div {
+  padding: 1rem;
+  border: 3px solid;
+  border-radius: Style.$default-border-radius;
+  box-shadow: 0.2rem 0.3rem 0.6rem rgba(0, 0, 0, 0.466);
+
+  & p {
+    font-size: Style.$font-2xl;
+  }
+}
+</style>
