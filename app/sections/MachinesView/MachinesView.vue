b4529c299817ddf322d6539c9698d6584b475eec
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-08 09:44:32 -0300

    feat: adiciona o estilo removido do arquivo de Style na tag do arquivo vue

--- app/sections/MachinesView/MachinesView.vue
+++ app/sections/MachinesView/MachinesView.vue
@@ -11,5 +11,36 @@
 <script setup lang="ts">
 import ImageComponent from "~/components/ImageComponent.vue";
 import MachinesList from "./components/MachinesList.vue";
-import "./MachinesViewStyle.scss";
 </script>
+
+<style lang="scss">
+@use "../../GlobalStyle.scss" as Style;
+
+.machinesview__div-container {
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+  padding: 2rem 1.5rem;
+}
+
+.machinesview__div-add-card {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 30rem;
+  cursor: pointer;
+  border-radius: Style.$default-border-radius;
+  background-color: Style.$purple-light;
+  box-shadow: 0.2rem 0.3rem 0.4rem rgba(0, 0, 0, 0.3);
+  transition: 300ms ease;
+
+  &:hover {
+    background-color: Style.$gray-dark;
+    transform: translateX(3px);
+  }
+
+  img {
+    width: 12rem;
+  }
+}
+</style>
