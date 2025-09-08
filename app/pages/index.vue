9aeade290216cce2ec03ca55f0d6219d36ed0d70
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-08 11:18:29 -0300

    fix: ajusta o nome do componente

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,14 +2,14 @@
   <NavBar />
   <NotificationProvider>
     <LoadingProvider>
-      <MachinesView />
+      <MachineView />
     </LoadingProvider>
   </NotificationProvider>
 </template>
 
 <script setup lang="js">
 import NavBar from "~/Sections/NavBar/NavBar.vue";
-import MachinesView from "~/Sections/MachinesView/MachinesView.vue";
+import MachineView from "~/Sections/MachinesView/MachineView.vue";
 import LoadingProvider from "~/Provider/LoadingProvider/LoadingProvider.vue";
 import NotificationProvider from "~/Provider/NotificationProvider/NotificationProvider.vue";
 </script>
