defacf8366f6a1e32e36a9a85ad3d6494cafc9b5
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-08 09:41:02 -0300

    refactor: remove os icon que não existem mais na aplicação

--- app/models/MachineStatus.ts
+++ app/models/MachineStatus.ts
@@ -1,7 +1,5 @@
-import type { Icons } from "./Icons";
-
-export const MachineStatus: Record<string, { label: string; icon: keyof typeof Icons; value: number }> = {
-  Online: { label: "Operante", icon: "OnlineFlagIcon", value: 1 },
-  Offline: { label: "Inoperante", icon: "OfflineFlagIcon", value: 2 },
-  Maintenance: { label: "Manutenção", icon: "MaintenanceFlagIcon", value: 3 },
+export const MachineStatus: Record<string, { label: string; value: number }> = {
+  Online: { label: "Operante", value: 1 },
+  Offline: { label: "Inoperante", value: 2 },
+  Maintenance: { label: "Manutenção", value: 3 },
 };
