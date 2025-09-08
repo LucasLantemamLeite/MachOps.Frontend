0fc907d1ee4f9c4b18bc32b512861df22575c32b
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-08 11:37:08 -0300

    chore: remove as pastas e deixa vai limpo na pasta de provider

---
+++ app/Provider/LoadingProvider.vue
@@ -0,0 +1,48 @@
+<template>
+  <div v-if="isLoading" class="loading__background">
+    <ImageComponent className="loading__spinner" staticImg="LoadingIcon" />
+  </div>
+
+  <slot></slot>
+</template>
+
+<script setup lang="ts">
+import ImageComponent from "~/components/ImageComponent.vue";
+
+const isLoading = ref(false);
+
+function setIsLoading(value: boolean) {
+  isLoading.value = value;
+}
+provide("loading", {
+  setIsLoading,
+});
+</script>
+
+<style lang="scss">
+.loading__background {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  inset: 0;
+  backdrop-filter: blur(3px);
+  background-color: rgba(0, 0, 0, 0.61);
+  z-index: 100;
+}
+
+.loading__spinner {
+  width: 25rem;
+  animation: animation-spinner 1s linear infinite;
+}
+
+@keyframes animation-spinner {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
---
+++ app/Provider/NotificationProvider.vue
@@ -0,0 +1,92 @@
+<template>
+  <div v-if="notification?.message" :class="['notifcation__card', isHidding ? 'hide' : 'visible']">
+    <ImageComponent :staticImg="handlerIconByType(notification.type)" />
+    {{ notification.message }}
+  </div>
+
+  <slot></slot>
+</template>
+
+<script setup lang="ts">
+import ImageComponent from "~/components/ImageComponent.vue";
+
+const notification = ref<{ message: string; type: "success" | "error" | "warning" | "info"; duration?: number } | null>(null);
+const isHidding = ref(false);
+
+let hideTimeout: number | undefined;
+let clearNotificationTimeout: number | undefined;
+
+function setNotification(message: string, type: "success" | "error" | "warning" | "info", duration: number = 5) {
+  if (hideTimeout) clearTimeout(hideTimeout);
+  if (clearNotificationTimeout) clearTimeout(clearNotificationTimeout);
+
+  isHidding.value = false;
+  notification.value = { message, type, duration };
+
+  hideTimeout = setTimeout(() => {
+    isHidding.value = true;
+  }, (duration - 1) * 1000);
+
+  clearNotificationTimeout = setTimeout(() => {
+    notification.value = null;
+    isHidding.value = false;
+  }, duration * 1000);
+}
+
+function handlerIconByType(type: string) {
+  switch (type) {
+    case "error":
+      return "CancelIcon";
+    case "warning":
+      return "WarningIcon";
+    case "info":
+      return "InfoIcon";
+    default:
+      return "ConfirmIcon";
+  }
+}
+
+provide("notification", {
+  setNotification,
+});
+</script>
+
+<style lang="scss">
+@use "../../GlobalStyle.scss" as Style;
+
+.notifcation__card {
+  position: fixed;
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  bottom: 1rem;
+  right: 1rem;
+  gap: 2rem;
+  padding: 2rem;
+  z-index: 2;
+  border-radius: Style.$default-border-radius;
+  width: 100%;
+  pointer-events: none;
+  max-width: 30rem;
+  background-color: Style.$gray-dark;
+  transform: translateX(20px);
+  transition: transform 200ms ease, opacity 0.3s ease;
+  opacity: 1;
+
+  & img {
+    width: 10rem;
+  }
+
+  &.visible {
+    opacity: 1;
+    transform: translateX(0px);
+  }
+
+  &.hide {
+    transform: translateX(20px);
+    opacity: 0;
+  }
+}
+</style>
