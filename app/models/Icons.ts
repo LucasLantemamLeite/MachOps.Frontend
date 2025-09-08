c6ad7b31a39bb0c2fc6a9c5317bc68d2538d1d37
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-08 12:16:45 -0300

    fix: ajusta rota de importação dos icones

--- app/models/Icons.ts
+++ app/models/Icons.ts
@@ -1,18 +1,18 @@
-import AppLogo from "~/Assets/Icons/AppLogo.svg";
-import Search from "~/Assets/Icons/Search.svg";
-import Add from "~/Assets/Icons/Add.svg";
-import Loading from "~/Assets/Icons/Loading.svg";
-import Confirm from "~/Assets/Icons/Confirm.svg";
-import Cancel from "~/Assets/Icons/Cancel.svg";
-import Warning from "~/Assets/Icons/Warning.svg";
-import Info from "~/Assets/Icons/Info.svg";
-import Forklift from "~/Assets/Machines/Forklift.svg";
-import Loader from "~/Assets/Machines/Loader.svg";
-import Excavator from "~/Assets/Machines/Excavator.svg";
-import Bulldozer from "~/Assets/Machines/Bulldozer.svg";
-import Crane from "~/Assets/Machines/Crane.svg";
-import AGV from "~/Assets/Machines/AGV.svg";
-import Skid from "~/Assets/Machines/Skid.svg";
+import AppLogo from "~/assets/icons/AppLogo.svg";
+import Search from "~/assets/icons/Search.svg";
+import Add from "~/assets/icons/Add.svg";
+import Loading from "~/assets/icons/Loading.svg";
+import Confirm from "~/assets/icons/Confirm.svg";
+import Cancel from "~/assets/icons/Cancel.svg";
+import Warning from "~/assets/icons/Warning.svg";
+import Info from "~/assets/icons/Info.svg";
+import Forklift from "~/assets/machines/Forklift.svg";
+import Loader from "~/assets/machines/Loader.svg";
+import Excavator from "~/assets/machines/Excavator.svg";
+import Bulldozer from "~/assets/machines/Bulldozer.svg";
+import Crane from "~/assets/machines/Crane.svg";
+import AGV from "~/assets/machines/AGV.svg";
+import Skid from "~/assets/machines/Skid.svg";
 
 export const Icons = {
   AppLogoIcon: AppLogo,
