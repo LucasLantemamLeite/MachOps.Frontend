9d1dccf60d0dd9e2d2da9b3a49a4c331cd84ebff
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-08 22:42:33 -0300

    feat: adiciona o estilo do arquivo global no app.vue

--- app/app.vue
+++ app/app.vue
@@ -4,5 +4,28 @@
 
 <script setup lang="ts">
 import { NuxtPage } from "#components";
-import "./GlobalStyle.scss";
 </script>
+
+<style lang="scss">
+@use "./styles/GlobalVariables.scss" as Var;
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  position: relative;
+  background-color: #ccc4c4;
+  font-size: Var.$font-2xl;
+  font-weight: bold;
+  color: white;
+  line-height: 1.4;
+  font-family: "Roboto", "Montserrat", sans-serif;
+}
+</style>
