6f6ddf218a8fe71eae260b4055807a31cf1a7f17
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-08 22:48:09 -0300

    feat: cria o componente de seleção

---
+++ app/components/SelectComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <label v-if="props.textLabel" :for="id">{{ props.textLabel }}</label>
+
+  <select v-if="props.type" :id="id" :name="name" :class="className">
+    <option v-for="obj in Object.values(props.type)" :key="obj.value">
+      {{ obj.label }}
+    </option>
+  </select>
+</template>
+
+<script setup lang="ts">
+import type { IconsModel } from "~/models/IconsModel";
+
+type typeObject = Record<string, { label: string; icon?: keyof typeof IconsModel; value: number }>;
+
+const props = defineProps<{
+  type: typeObject;
+  name?: string;
+  id?: string;
+  textLabel?: string;
+  className?: string;
+}>();
+</script>
