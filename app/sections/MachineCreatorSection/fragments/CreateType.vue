3f272e884a8ef84694565542a1d6e4fe00750a18
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-08 22:50:43 -0300

    feat: cria os fragmentos para a seção de criar novas máquinas

---
+++ app/sections/MachineCreatorSection/fragments/CreateType.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <SelectComponent name="type" id="type" textLabel="Tipo: " :type="MachineTypeModel" className="machine-creator__type" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { MachineTypeModel } from "~/models/MachineTypeModel";
+import SelectComponent from "~/components/SelectComponent.vue";
+</script>
+
+<style lang="scss">
+@use "../../../styles/GlobalVariables.scss" as Var;
+
+.machine-creator__type {
+  width: 35rem;
+  padding: 0.9rem;
+  border: none;
+  font-size: 1.6rem;
+  font-weight: inherit;
+  font-family: inherit;
+  outline: none;
+  border-radius: Var.$default-border-radius;
+}
+</style>
