2a81429634e7ad0961283ed94a1929caaca9918f
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-09 23:28:47 -0300

    feat: cria o componente de textarea para entrada de dados maiores

---
+++ app/components/TextAreaComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <label v-if="textLabel && id" :for="id">{{ textLabel }}</label>
+  <textarea :class="className" :id="id" :name="name" :type="type" :placeHolder="placeHolder" :maxlength="maxLenght" :autocomplete="autoComplete" />
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    id?: string;
+    name?: string;
+    type?: string;
+    textLabel?: string;
+    placeHolder?: string;
+    className?: string;
+    maxLenght?: number;
+    autoComplete?: "on" | "off";
+  }>(),
+  {
+    type: "text",
+    placeHolder: "",
+    value: "",
+    autoComplete: "off",
+  }
+);
+</script>
