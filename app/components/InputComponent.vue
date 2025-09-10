7720d9d7d4046f9dd1be936ef9787977d4f17371
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-09 21:53:22 -0300

    feat: adiciona novas propriedades e permite passar funções para alterar dados como valores de ref do vue

--- app/components/InputComponent.vue
+++ app/components/InputComponent.vue
@@ -1,10 +1,10 @@
 <template>
   <label v-if="textLabel && id" :for="id">{{ textLabel }}</label>
-  <input :class="className" :id="id" :name="name" :type="type" :placeHolder="placeHolder" />
+  <input :class="className" :id="id" :name="name" :type="type" :placeHolder="placeHolder" :value="value" @input="handlerChange" :maxlength="maxLenght" :autocomplete="autoComplete" />
 </template>
 
 <script setup lang="ts">
-withDefaults(
+const props = withDefaults(
   defineProps<{
     id?: string;
     name?: string;
@@ -12,10 +12,22 @@ withDefaults(
     textLabel?: string;
     placeHolder?: string;
     className?: string;
+    value?: string;
+    maxLenght?: number;
+    autoComplete?: "on" | "off";
+    onChange?: (value: string) => void;
   }>(),
   {
     type: "text",
     placeHolder: "",
+    value: "",
+    autoComplete: "off",
   }
 );
+
+function handlerChange(event: Event) {
+  const target = event.target as HTMLInputElement;
+
+  props.onChange?.(target.value);
+}
 </script>
