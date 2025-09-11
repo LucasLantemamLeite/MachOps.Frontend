14ba745089cd86ae9a231e902c1cbf6d6b683973
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-10 23:09:57 -0300

    feat: cria o componente de botão

---
+++ app/components/ButtonComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <button :class="className" @click="$emit('click')">
+    <ImageComponent v-if="icon" :staticImg="icon" />
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+import ImageComponent from "./ImageComponent.vue";
+import type { IconsModel } from "~/models/IconsModel";
+
+defineProps<{
+  icon?: keyof typeof IconsModel;
+  className?: string;
+}>();
+
+defineEmits<{ (e: "click"): void }>();
+</script>
