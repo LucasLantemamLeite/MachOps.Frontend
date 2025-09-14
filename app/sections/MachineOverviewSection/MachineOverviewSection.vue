0518b348433db4879e0cf716e10e814edd68859b
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-14 10:52:03 -0300

    feat: adiciona a lógica para abrir o modal da máquina populada ou vazia

--- app/sections/MachineOverviewSection/MachineOverviewSection.vue
+++ app/sections/MachineOverviewSection/MachineOverviewSection.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="machine-overview__container">
-    <div @click="isMachineCreaterOpen = true" class="machine-overview__add-card">
+    <div
+      @click="
+        () => {
+          isMachineCreaterOpen = true;
+          selectedMachine = null;
+        }
+      "
+      class="machine-overview__add-card"
+    >
       <ImageComponent staticImg="AddIcon" />
     </div>
 
-    <ViewCard :filter="filter" />
+    <ViewCard :filter="filter" @selectMachine="handleSelectMachine" />
 
-    <MachineCreatorSection :setIsOpen="(v) => (isMachineCreaterOpen = v)" v-if="isMachineCreaterOpen" />
+    <MachineCreatorSection :setIsOpen="(v) => (isMachineCreaterOpen = v)" v-if="isMachineCreaterOpen" :machine="selectedMachine" />
   </div>
 </template>
 
@@ -15,8 +23,16 @@ import ImageComponent from "~/components/ImageComponent.vue";
 import MachineCreatorSection from "../MachineCreatorSection/MachineCreatorSection.vue";
 import ViewCard from "./fragments/ViewCard.vue";
 import "./MachineOverViewStyle.scss";
+import type { Machine } from "~/models/MachineModel";
 
 const isMachineCreaterOpen = ref(false);
+
+const selectedMachine = ref<Machine | null>(null);
+
+function handleSelectMachine(machine: Machine) {
+  selectedMachine.value = machine;
+  isMachineCreaterOpen.value = true;
+}
 
 defineProps<{ filter?: string }>();
 </script>
