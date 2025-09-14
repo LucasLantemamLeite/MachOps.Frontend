77a52ec33eeb17a78cdcac181297239a0f10ee1e
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-14 10:48:57 -0300

    feat: adiciona prop para definir um valor já selecionado e verificação

--- app/components/TextAreaComponent.vue
+++ app/components/TextAreaComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <label v-if="textLabel && id" :for="id">{{ textLabel }}</label>
-  <textarea :class="className" :id="id" :name="name" :type="type" :placeHolder="placeHolder" :maxlength="maxLenght" :autocomplete="autoComplete" />
+  <textarea :class="className" :id="id" :name="name" :type="type" :placeHolder="placeHolder" :maxlength="maxLenght" :autocomplete="autoComplete" :value="value ?? ''" />
 </template>
 
 <script setup lang="ts">
@@ -14,6 +14,7 @@ withDefaults(
     className?: string;
     maxLenght?: number;
     autoComplete?: "on" | "off";
+    value?: string | null;
   }>(),
   {
     type: "text",
