e3f3bdf9f35a4c5f2cacd5789384f07a04ca3124
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-14 09:27:05 -0300

    feat: adiciona o estado de filtro

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,21 @@
 <template>
-  <NavBarSection />
+  <NavBarSection :filter="filter" :setFilter="setFilter" />
   <NotificationProvider>
     <LoadingProvider>
-      <MachineOverviewSection />
+      <MachineOverviewSection :filter="filter" />
     </LoadingProvider>
   </NotificationProvider>
 </template>
 
-<script setup lang="js">
+<script setup lang="ts">
 import NavBarSection from "~/layouts/NavBarLayout.vue";
 import MachineOverviewSection from "~/sections/MachineOverviewSection/MachineOverviewSection.vue";
 import LoadingProvider from "~/providers/LoadingProvider.vue";
 import NotificationProvider from "~/providers/NotificationProvider.vue";
+
+const filter = ref("");
+
+function setFilter(search: string) {
+  filter.value = search;
+}
 </script>
