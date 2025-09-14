78308a16cdb4a9e144616a355a2847cc147eb6bd
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-13 23:20:23 -0300

    refactor: adiciona os mixins globais

--- app/sections/MachineCreatorSection/fragments/MachPreview.vue
+++ app/sections/MachineCreatorSection/fragments/MachPreview.vue
@@ -24,29 +24,23 @@ defineProps<{
 </script>
 
 <style lang="scss">
+@use "../../../styles/GlobalMixins.scss" as Mix;
 @use "../../../styles/GlobalVariables.scss" as Var;
 
 .creator-machine__preview {
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 1rem;
+  @include Mix.div-field-base($display: flex, $direction: column, $gap: 1rem);
+  @include Mix.position-field-base($position: relative);
+  @include Mix.align-field-base($justify: center, $align: center);
+  @include Mix.colors-field-base($background: Var.$purple-primary);
+  @include Mix.box-frame-field-base($radius: Var.$default-border-radius, $border: 1px solid white);
+  @include Mix.spacing-field-base($padding: 1rem);
+  @include Mix.size-field-base($width: 37rem);
   margin-bottom: 2rem;
-  background-color: Var.$purple-primary;
-  padding: 1rem;
-  border: 1px solid white;
   box-shadow: 0.2rem 0.3rem 0.6rem rgba(0, 0, 0, 0.411);
-  width: 37rem;
-  border-radius: Var.$default-border-radius;
 }
 
 .creator-machine__info {
-  position: absolute;
-  top: -2.8rem;
-  font-weight: inherit;
-  font-family: inherit;
-  left: 0rem;
+  @include Mix.position-field-base($position: absolute, $top: -2.8rem, $left: 0rem);
+  @include Mix.font-field-base();
 }
 </style>
