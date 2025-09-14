eff0c8e1c47ce524ce6c609a70cd3e08bfd3473f
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-09-13 21:26:02 -0300

    feat: adiciona nova rota na central de rotas do servidor

--- app/models/ApiRouterModel.ts
+++ app/models/ApiRouterModel.ts
@@ -1,4 +1,5 @@
 export const ApiRouterModel = {
   HealthCheck: { path: "/health", method: "GET" },
   GetAllMachines: { path: "/machines", method: "GET" },
+  CreateNewMachine: { path: "/machines", method: "POST" },
 };
